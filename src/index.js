83f6151bafc304f3c270a2b10869ac346ca79a3c
Muyumba <Muyumba.Musafiri@ulb.be>
2025-12-01 01:32:13 +0100

    Fix route /index  per errore pagina bianca

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // Pour les styles globaux
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 // Point d'entrée de l'application
